e1897e52866594d5f2869711939aac6ebad43caa
OnufreiArtem <2hilderoy2@gmail.com>
2020-08-04 22:24:48 +0300

    save sectors of image as region

--- sortigo/builder.py
+++ sortigo/builder.py
@@ -8,21 +8,24 @@ from . exceptions import UnknownAlgorithmException
 
 def build_frame(divider:Separator):
     buffer_image = Image.new('RGBA', ( divider.image_width, divider.image_height ), (0, 0, 0, 0))
-    draw = ImageDraw.Draw(buffer_image)
-    for x in range(buffer_image.width):
-        for y in range(buffer_image.height):
-            sector_indexes = divider.detect_sector(x, y)
-            sector_indexes['x'] -= 1
-            sector_indexes['y'] -= 1
-            current_sector = dict(x=divider.row_arrays[sector_indexes['y']][sector_indexes['x']], y=sector_indexes['y'])
-            local_coords = divider.get_local_coords(x, y)
-            draw.point((x, y), divider.get_selected_pixel(current_sector, local_coords) )
-    
+    #print(divider.row_arrays)
+    for sector_y in range(len(divider.row_arrays)):
+        for sector_x in range(divider.columns):
+            #print( "column {0} row {1} ".format(divider.row_arrays[sector_y][sector_x], sector_y) )
+            sector = divider.get_sector(sector_y,divider.row_arrays[sector_y][sector_x])
+            #print(sector)
+            buffer_image.paste(sector['region'], (sector_x*divider.sector_width, sector_y*divider.sector_width, (sector_x+1)*divider.sector_width, (sector_y+1)*divider.sector_width))
+
     return buffer_image
 
 def build_animation(image_path:str, settings:dict, video_name:str, extention:str):
+    import time
+
+    start_time = time.time()
     sep = Separator(image_path, settings)
-    
+
+    out = cv2.VideoWriter(video_name + '.' + extention, cv2.VideoWriter_fourcc(*'DIVX'), 15, (sep.image_width, sep.image_height))
+
     algorithm = None
     if settings['algorithm'] == "Bubble":
         algorithm = BubbleSemiSort()
@@ -34,12 +37,20 @@ def build_animation(image_path:str, settings:dict, video_name:str, extention:str
     for row in sep.row_arrays:
         steps.append(algorithm.get_all_iterations(row))
 
-    import numpy 
+
+    import os
+
     i = 0
     for phase in range(len(steps[0])):
         for row in range(len(sep.row_arrays)):
             sep.row_arrays[row] = steps[row][phase]
         frame = build_frame(sep)
-        frame.save('frames/frame_{0}.png'.format(i), "PNG")
+        filepath = 'frames/frame_{0}.png'.format(i)
+        frame.save(filepath, "PNG")
+        out.write( cv2.imread(filepath) )
+        os.remove(filepath)
         i+=1
 
+    out.release()
+
+    print('It took ' + str(time.time() - start_time))
