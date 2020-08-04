5a91c0059d79a8765eb1edbe76d8e17db05eafc1
OnufreiArtem <2hilderoy2@gmail.com>
2020-08-04 14:59:28 +0300

    added functionality to build_animation

--- sortigo/builder.py
+++ sortigo/builder.py
@@ -4,6 +4,7 @@ import cv2
 
 from . separator import Separator
 from . sorting import Sorting, BubbleSemiSort
+from . exceptions import UnknownAlgorithmException
 
 def build_frame(divider:Separator):
     buffer_image = Image.new('RGBA', ( divider.image_width, divider.image_height ), (0, 0, 0, 0))
@@ -20,14 +21,25 @@ def build_frame(divider:Separator):
     return buffer_image
 
 def build_animation(image_path:str, settings:dict, video_name:str, extention:str):
-    sep = Separator(image_path, image_settings)
+    sep = Separator(image_path, settings)
     
     algorithm = None
     if settings['algorithm'] == "Bubble":
         algorithm = BubbleSemiSort()
     else:
-        raise 
+        raise UnknownAlgorithmException("Unknown or undefined type of algorithm was specified") 
+    
     steps = []
     for row in sep.row_arrays:
         algorithm.set_array(row)     
-        steps.append( algorithm.get_all_iterations )
+        steps.append( algorithm.get_all_iterations() )
+
+    import numpy 
+    i = 0
+    for phase in range(len(steps[0])):
+        for row in range(len(sep.row_arrays)):
+            sep.row_arrays[row] = steps[row][phase]
+        frame = build_frame(sep)
+        frame.save('frames/frame_{0}.png'.format(i), "PNG")
+        i+=1
+
