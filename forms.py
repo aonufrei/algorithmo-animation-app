d9b5f194919d15b8bd2dec4f103e8da73c623f9b
OnufreiArtem <2hilderoy2@gmail.com>
2020-08-09 16:10:34 +0300

    added form

--- forms.py
+++ forms.py
@@ -1,4 +1,13 @@
 from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField
+from wtforms.validators import InputRequired, AnyOf
 
 class ImageUploaderForm(FlaskForm):
-    pass
+    image_width = IntegerField('image_width', validators=[InputRequired('Please, input image width')])
+    image_height = IntegerField('image_height', validators=[InputRequired('Please, input image width')])
+
+    image_columns = IntegerField('image_width', validators=[InputRequired('Please, input image width')])
+    image_rows = IntegerField('image_width', validators=[InputRequired('Please, input image width')])
+
+    algorithm = StringField('algorithm', validators=[InputRequired('Pleasem input algorithm to process image'), AnyOf(values=['Bubble'])])
+    
